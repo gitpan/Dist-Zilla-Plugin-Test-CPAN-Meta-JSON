65207e04d3ba6c7fab212ed0dfb9bf3ebb4d53d6
Mike Doherty <doherty@cpan.org>
2011-04-18 02:01:49 +0000

    Import of DOHERTY/Dist-Zilla-Plugin-Test-CPAN-Meta-JSON-0.002 from CPAN.
    gitpan-cpan-distribution: Dist-Zilla-Plugin-Test-CPAN-Meta-JSON
    gitpan-cpan-version:      0.002
    gitpan-cpan-path:         DOHERTY/Dist-Zilla-Plugin-Test-CPAN-Meta-JSON-0.002.tar.gz
    gitpan-cpan-author:       DOHERTY
    gitpan-cpan-maturity:     released

--- lib/Dist/Zilla/Plugin/Test/CPAN/Meta/JSON.pm
+++ lib/Dist/Zilla/Plugin/Test/CPAN/Meta/JSON.pm
@@ -2,12 +2,33 @@ package Dist::Zilla::Plugin::Test::CPAN::Meta::JSON;
 use strict;
 use warnings;
 # ABSTRACT: release tests for your META.json
-our $VERSION = '0.001'; # VERSION
+our $VERSION = '0.002'; # VERSION
 
 use Moose;
-use Moose;
+use Moose::Autobox;
 extends 'Dist::Zilla::Plugin::InlineFiles';
+with 'Dist::Zilla::Role::FilePruner';
 
+
+sub prune_files {
+    my $self = shift;
+
+    # Bail if we find META.json
+    my $METAjson = 'META.json';
+    foreach my $file ($self->zilla->files->flatten) {
+        return if $file->name eq $METAjson;
+    }
+
+    # If META.json wasn't found, then prune out the test
+    my $test_filename = 'xt/release/meta-json.t';
+    foreach my $file ($self->zilla->files->flatten) {
+        next unless $file->name eq $test_filename;
+
+        $self->zilla->prune_file($file);
+        $self->log_debug([ '%s not found; pruning %s', $METAjson, $file->name ]);
+    }
+    return;
+}
 
 __PACKAGE__->meta->make_immutable;
 no Moose;
@@ -27,7 +48,7 @@ Dist::Zilla::Plugin::Test::CPAN::Meta::JSON - release tests for your META.json
 
 =head1 VERSION
 
-version 0.001
+version 0.002
 
 =head1 SYNOPSIS
 
@@ -38,16 +59,20 @@ In C<dist.ini>:
 =head1 DESCRIPTION
 
 This is an extension of L<Dist::Zilla::Plugin::InlineFiles>, providing the
-following file:
+following file if F<META.json> is in your dist:
 
     xt/release/meta-json.t - a standard Test::CPAN::Meta::JSON test
 
 See L<Test::CPAN::Meta::JSON> for what this test does.
 
+=for Pod::Coverage prune_files
+
 =for test_synopsis 1;
 __END__
 
 =head1 AVAILABILITY
+
+The project homepage is L<http://p3rl.org/Dist::Zilla::Plugin::Test::CPAN::Meta::JSON>.
 
 The latest version of this module is available from the Comprehensive Perl
 Archive Network (CPAN). Visit L<http://www.perl.com/CPAN/> to find a CPAN
@@ -68,7 +93,7 @@ and may be cloned from L<git://github.com/doherty/Dist-Zilla-Plugin-Test-CPAN-Me
 No bugs have been reported.
 
 Please report any bugs or feature requests through the web interface at
-L<http://github.com/doherty/Dist-Zilla-Plugin-Test-CPAN-Meta-JSON/issues>.
+L<https://github.com/doherty/Dist-Zilla-Plugin-Test-CPAN-Meta-JSON/issues>.
 
 =head1 AUTHOR
 
